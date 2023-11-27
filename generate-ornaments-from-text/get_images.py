efb95277c05cd8128c83f9e978b9b33ef2a06ee2
Daniel Cardoza <dan@danielcardoza.com>
2023-11-27 01:26:49 -0500

    s

--- generate-ornaments-from-text/get_images.py
+++ generate-ornaments-from-text/get_images.py
@@ -1,58 +1,54 @@
 import os
+import pathlib
 import requests
 from openai import OpenAI
 import concurrent.futures
+import datetime
 
 client = OpenAI()
 
 # List of image prompts
 objects = [
-    "white siamese cat",
-    "red rose with stem",
-    "BMW supercar",
-    "Mario from Super Mario Bros",
-    "belgian malinois dog",
+    "a belgian malinois",
+    "a malinois",
+    "a malinois from the side standing with their tail behind them"
 ]
 
-# Folder to save the images
-output_folder = "/home/dang3r/dev/forge/generate-ornaments-from-text/images"
-# make a directory even if it already exists
-if not os.path.exists(output_folder):
-    os.mkdir(output_folder)
+
+# Define the number of" workers
+num_workers = 3
 
 
-# Define the number of workers
-num_workers = 4
+def download_and_save_image(object: str, index: int) -> None:
+    try:
+        prompt = f"""A 3D model of a single (only one) `{object}` whose full body is visible from the front.
+        Make sure all of the object is visible and in frame. Please only include the object and nothing else!
+        Make sure the edges of the object are sharp and not too blurry.
+        """
+        timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%d-%H-%M-%S")
+        norm_prompt = '_'.join(object.split()) 
+        file_path = pathlib.Path(f"./images/{timestamp}-{norm_prompt}.png")
 
-# Function to download and save the image
-def download_and_save_image(object, index):
-    prompt = f"a 3d model of a single (only one) {object} whose full body is visible from the front left. Make sure the side and back of the object is visible. Please only include the object and nothing else!"
-    # Make API call to generate the image
-    response = client.images.generate(
-        model="dall-e-3",
-        prompt=prompt,
-        size="1024x1024",
-        quality="standard",
-        n=1,
-    )
-    print(f"Generated image for prompt: `{prompt}`")
+        print(f"Generating image for prompt: `{prompt}`")
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        print(f"Generated image for prompt: `{prompt}`")
 
-    # Retrieve the image URL from the response
-    image_url = response.data[0].url
+        image_url = response.data[0].url
+        image_data = requests.get(image_url).content
+        with open(file_path, "wb") as f:
+            f.write(image_data)
+    except Exception as error:
+        print(error)
 
-    # Download the image from the URL
-    image_data = requests.get(image_url).content
-
-    # Save the image to a folder with a unique filename
-    tokens = object.split()
-    filename = f"{'_'.join(tokens)}.png"
-    filepath = os.path.join(output_folder, filename)
-    with open(filepath, "wb") as f:
-        f.write(image_data)
 
 # Create a ThreadPoolExecutor with the specified number of workers
 with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-    # Iterate over each image prompt
     for i, prompt in enumerate(objects):
-        # Submit the download_and_save_image function as a task to the executor
+        print(i, prompt)
         executor.submit(download_and_save_image, prompt, i)
