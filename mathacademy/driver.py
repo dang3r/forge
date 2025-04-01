07ad369765bb4c9015141cff5f5b8907d67cf906
Daniel Cardoza <danielpcardoza@gmail.com>
2025-03-31 22:34:27 -0400

    Initial experiments with the Model Context Protocol (#7)
    - Initial MCP work that heavily uses the tutorial
    - Some other Math Academy stuff

---
+++ mathacademy/driver.py
@@ -0,0 +1,13 @@
+import json
+import os
+from api import MathAcademyAPI
+from generate_graph import generate_all_graphs
+
+pw = os.getenv("MATHACADEMY_PASSWORD")
+email = os.getenv("MATHACADEMY_EMAIL")
+api = MathAcademyAPI()
+api.login(email, pw)
+
+print(json.dumps(api.get_student_xp_goals("4760"), indent=4))
+knowledge_graph = api.get_student_course_knowledge_graph("4760", ["111"])
+generate_all_graphs(knowledge_graph)
